1b41e02c6bf30eb490d80aa09fa847ac2e85d236
vadim moskvin <vadim.moskvin@effective-soft.com>
2019-06-21 10:40:43 +0300

    CU541-97 Application crashes if pressing "Finish" without entering name

---
+++ CHAD Model/DesktopApplication/Converters/FinishEnabledConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CHAD.DesktopApplication.Converters
+{
+    public class FinishEnabledConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if(values.Length < 2)
+                throw new ArgumentException(nameof(values));
+
+            var navigationServiceNextEnabled = (bool)values[0];
+            var name = (string) values[1];
+
+            return navigationServiceNextEnabled && !string.IsNullOrEmpty(name);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
